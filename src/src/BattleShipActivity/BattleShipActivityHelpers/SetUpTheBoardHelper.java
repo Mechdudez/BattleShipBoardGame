0c94e7d682e2fe3e8b2be33623a83d6ebbc1a399
Ryan Wiest <james.wiest@mykenzie.snhu.edu>
2022-06-17 22:09:24 -0700

    push

--- src/src/BattleShipActivity/BattleShipActivityHelpers/SetUpTheBoardHelper.java
+++ src/src/BattleShipActivity/BattleShipActivityHelpers/SetUpTheBoardHelper.java
@@ -16,67 +16,34 @@ public class SetUpTheBoardHelper {
 
         System.out.println(makeTheFirstRow);
     }
-
-    public void getPlayerTwoMap() {
-
-        for (int j = 0; j < 11; j++) {
-
-            System.out.print(" |");
-        }
-    }
-
-    public void getPlayerOneMap() {
-        for (int i = 0; i < 11; i++) {
-
-            System.out.print(" |");
-
-        }
-//        System.out.print("                   ");
-//
-//        for (int j = 0; j < 11; j ++){
-//            System.out.print(" |");
-//        }
-
-    }
-
-    public void getUpAndDown() {
-        for (int i = 0; i < 11; i++) {
-            System.out.print("   _");
-        }
-    }
-
-    public void getNumbers() {
-
-//        for(int i = 0; i < 10; i++){
-//            System.out.println(i);
-//        }
-
-//        for(int j = 0; j < 10; j++){
-//            System.out.println("                                                  " + j);
-//        }
+    public void getNumbers(){
 
     }
 
     public void setUpTheBoard() {
+        String[] numbers = {" ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String [] letters = {" ", " A", " B", " C", " D", " E", " F", " G", " H", " I", " J"};
+        // First iteration has to be the number
+        for (int i = 0; i < 11; i++){
+            for(int j = 0; j < 11; j++){
+                if(i == 0){
+                    System.out.print(letters[j]);
 
-        getFirstRow();
-        for (int i = 0; i < 10; i++) {
-            //  getUpAndDown();
-
-            System.out.print(i);
-            for (int j = 0; j < 11; j++) {
-
-                System.out.print(" |");
-
+                }else if(j == 0){
+                    System.out.print(numbers[i]);
+                }else {
+                    System.out.print(" |");
+                }
 
             }
-            
-            System.out.print("                   ");
-            System.out.println(i);
-
-
+            System.out.println();
         }
+        // there has to be 11 iterations to make sure there is 10 spots to shoot.
 
+        // Each spot will look like a box, how do you make sure that happens? Use | and _ together.
+        // have to also be able to change what is in the box to an S/H or X
     }
 
 }
+
+
